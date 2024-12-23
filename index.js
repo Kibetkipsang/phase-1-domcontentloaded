04b823e24e1361b6c672bca649ddf0db82d347d5
Kibet Kipsang <denniskipsang2000@gmail.com>
2024-12-23 11:20:12 +0300

    inedx.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", () => {console.log("The Dom has loaded")});
+// console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
+  
+
+document.addEventListener("DOMContentLoaded", () => {
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = ('This is really cool!')   
+  })
